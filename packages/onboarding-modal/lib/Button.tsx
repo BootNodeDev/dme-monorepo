22dad3ba29a3e68423df523e50597fd7d2bf32e3
Nando <24811313+fzavalia@users.noreply.github.com>
2025-10-08 17:04:56 -0300

    fix: Bell icon

--- packages/onboarding-modal/lib/Button.tsx
+++ packages/onboarding-modal/lib/Button.tsx
@@ -30,9 +30,9 @@ export function Button({ children, modal }: MyButtonProps) {
 function BellIcon() {
   return (
     <svg
-      width="24"
-      height="24"
-      viewBox="0 0 20 20"
+      width="20"
+      height="20"
+      viewBox="0 0 24 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       className="dme-modal__bell-icon"
