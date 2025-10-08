7ff781223e4ae817e0191c1cc67c46f7296e6a24
Nando <24811313+fzavalia@users.noreply.github.com>
2025-10-08 15:47:30 -0300

    fix: Dont telegramify on limiter

--- packages/server/src/limiter.ts
+++ packages/server/src/limiter.ts
@@ -2,7 +2,6 @@ import { Bot, Context, SessionFlavor } from "grammy";
 import { ParseMode } from "grammy/types";
 import PQueue from "p-queue";
 import { Logger } from "pino";
-import telegramify from "telegramify-markdown";
 
 export const TOP_PRIORITY = 999;
 export const LOW_PRIORITY = 1;
@@ -29,13 +28,13 @@ export class Limiter {
   }
 
   reply(ctx: Context, msg: string) {
-    this.add(TOP_PRIORITY, () => ctx.reply(telegramify(msg, "remove"), this.BASE_MESSAGE_OPTIONS));
+    this.add(TOP_PRIORITY, () => ctx.reply(msg, this.BASE_MESSAGE_OPTIONS));
   }
 
   sendMessage(chatId: number, msg: string, onSuccess: () => void, onError: (error: Error) => void) {
     this.add(LOW_PRIORITY, () =>
       this.bot.api
-        .sendMessage(chatId, telegramify(msg, "remove"), this.BASE_MESSAGE_OPTIONS)
+        .sendMessage(chatId, msg, this.BASE_MESSAGE_OPTIONS)
         .then(onSuccess)
         .catch(onError),
     );
