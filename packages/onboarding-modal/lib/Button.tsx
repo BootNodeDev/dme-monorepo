d9caef5856b52ce36ab2889f7f71cebf528b4c70
Nando <24811313+fzavalia@users.noreply.github.com>
2025-10-08 16:58:40 -0300

    fix: Update size

--- packages/onboarding-modal/lib/Button.tsx
+++ packages/onboarding-modal/lib/Button.tsx
@@ -32,7 +32,7 @@ function BellIcon() {
     <svg
       width="24"
       height="24"
-      viewBox="0 0 24 24"
+      viewBox="0 0 20 20"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       className="dme-modal__bell-icon"
