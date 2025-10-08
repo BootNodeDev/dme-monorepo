ad2f428d39c484ae5dabf5ec0c1088e3c3b3025f
Nando <24811313+fzavalia@users.noreply.github.com>
2025-10-08 09:28:06 -0300

    feat: Add getsendmessage fn

--- packages/server/src/limiter.spec.ts
+++ packages/server/src/limiter.spec.ts
@@ -1,7 +1,7 @@
 import PQueue from "p-queue";
-import { getLimiter, getReplyFn } from "./limiter";
-import { Context } from "grammy";
-import { MESSAGE_CONTENT } from "./tests/constants";
+import { BASE_OPTIONS, getLimiter, getReplyFn, getSendMessageFn } from "./limiter";
+import { Bot, Context } from "grammy";
+import { MESSAGE_CONTENT, USER_ID_1 } from "./tests/constants";
 
 jest.mock("p-queue", () =>
   jest.fn().mockImplementation(() => ({
@@ -35,9 +35,23 @@ describe("getReplyFn", () => {
     getReplyFn(limiter)(ctx, MESSAGE_CONTENT);
 
     expect(limiter.add).toHaveBeenCalledWith(expect.any(Function));
-    expect(ctx.reply).toHaveBeenCalledWith(MESSAGE_CONTENT, {
-      parse_mode: "MarkdownV2",
-      link_preview_options: { is_disabled: true },
-    });
+    expect(ctx.reply).toHaveBeenCalledWith(MESSAGE_CONTENT, BASE_OPTIONS);
+  });
+});
+
+describe("getSendMessageFn", () => {
+  it("should return a function that adds a sendMessage task to the limiter", () => {
+    const limiter = getLimiter(interval, intervalCap);
+
+    const mockBot = {
+      api: {
+        sendMessage: jest.fn(),
+      },
+    } as unknown as jest.Mocked<Bot>;
+
+    getSendMessageFn(limiter, mockBot)(USER_ID_1, MESSAGE_CONTENT);
+
+    expect(limiter.add).toHaveBeenCalledWith(expect.any(Function));
+    expect(mockBot.api.sendMessage).toHaveBeenCalledWith(USER_ID_1, MESSAGE_CONTENT, BASE_OPTIONS);
   });
 });
--- packages/server/src/limiter.ts
+++ packages/server/src/limiter.ts
@@ -1,8 +1,16 @@
-import { Context } from "grammy";
+import { Bot, Context } from "grammy";
+import { ParseMode } from "grammy/types";
 import PQueue from "p-queue";
 import telegramify from "telegramify-markdown";
 
+export const BASE_OPTIONS = {
+  parse_mode: "MarkdownV2" as ParseMode,
+  link_preview_options: { is_disabled: true },
+};
+
 export type ReplyFn = (ctx: Context, msg: string) => void;
+
+export type SendMessageFn = (chatId: number, msg: string) => void;
 
 export function getLimiter(interval: number, intervalCap: number): PQueue {
   return new PQueue({
@@ -14,11 +22,12 @@ export function getLimiter(interval: number, intervalCap: number): PQueue {
 
 export function getReplyFn(limiter: PQueue): ReplyFn {
   return (ctx: Context, msg: string) => {
-    limiter.add(() =>
-      ctx.reply(telegramify(msg, "remove"), {
-        parse_mode: "MarkdownV2",
-        link_preview_options: { is_disabled: true },
-      }),
-    );
+    limiter.add(() => ctx.reply(telegramify(msg, "remove"), BASE_OPTIONS));
+  };
+}
+
+export function getSendMessageFn(limiter: PQueue, bot: Bot): SendMessageFn {
+  return (chatId: number, msg: string) => {
+    limiter.add(() => bot.api.sendMessage(chatId, telegramify(msg, "remove"), BASE_OPTIONS));
   };
 }
