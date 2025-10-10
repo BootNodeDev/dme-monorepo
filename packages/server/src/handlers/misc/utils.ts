bc5802a55d07519d04f531bda55ca2543f216ce2
Nando <24811313+fzavalia@users.noreply.github.com>
2025-10-10 00:12:34 -0300

    fix: Add test

--- packages/server/src/handlers/misc/utils.ts
+++ packages/server/src/handlers/misc/utils.ts
@@ -8,16 +8,6 @@ export function formatAddress(address: string) {
   return `${address.slice(0, 6)}...${address.slice(-4)}`;
 }
 
-export function getUserIdFromCtx(ctx: CommandContext<Context>) {
-  const userId = ctx.from?.id;
-
-  if (!userId) {
-    throw new Error("No user ID found in the context");
-  }
-
-  return userId;
-}
-
 export function baseHandler(
   logger: Logger,
   message: MessageService,
