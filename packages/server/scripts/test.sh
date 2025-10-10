67a3a89489cfb10804e7a1eff23dd70b57b0ee76
Nando <24811313+fzavalia@users.noreply.github.com>
2025-10-10 00:20:40 -0300

    fix: Skip generate

--- packages/server/scripts/test.sh
+++ packages/server/scripts/test.sh
@@ -5,11 +5,14 @@ set -e
 # Set the database url environment variable for the tests
 export DATABASE_URL="file:./test.db"
 
+# Generate the Prisma client
+pnpm prisma generate
+
 # Check for compilation errors
 pnpm tsc --noEmit
 
 # Setup the database schema
-pnpm prisma db push --force-reset
+pnpm prisma db push --force-reset --skip-generate
 
 # Run tests, and exit with the test results
 pnpm jest --coverage --runInBand --verbose "$@"
