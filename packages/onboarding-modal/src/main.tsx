b17c39b0d6c24385dcd85c5b0cedf9af9bfca396
Nando <24811313+fzavalia@users.noreply.github.com>
2025-10-16 11:12:51 -0300

    fix: Update styles

--- packages/onboarding-modal/src/main.tsx
+++ packages/onboarding-modal/src/main.tsx
@@ -1,17 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Button } from "../lib";
-import "../lib/styles/alt1.css";
+import "../lib/index.css";
 import "./main.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Button
       modal={{
-        bot: "MyDeFiBot",
+        bot: "DMe_AlertsBot",
         address: "0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045",
         title: "Welcome to DMe!",
-        description: "Scan the QR code or click the button below to get started.",
+        description:
+          "Get real-time DeFi alerts and notifications directly to your Telegram. Scan the QR code or click the button below to get started.",
         cta: "Open Telegram",
       }}
     >
